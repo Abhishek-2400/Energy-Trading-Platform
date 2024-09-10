a293c172dffc6f0c739832a07fbde467c17017fc
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-09-10 05:36:57 +0530

    Landing Page

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-heading">EnergyHive</div>
+            <ul className="navbar-menu">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#marketplace">Market Place</a></li>
+            </ul>
+        </nav>
+    );
+}
